d67f6b428cd8bfda3dbb46f1eec53aaaa30ae968
vien pham <vienpham2019@gmail.com>
2019-12-10 02:20:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,7 +46,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     hash[key] = hash[key]++
-  else
-    hash[key] = 1
-  end
+    else
+      hash[key] = 1
+    end
 end
